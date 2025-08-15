978192fa4f0eae3b6d0c3074b452673f0354b354
David Roman <officialdavidromanyt@gmail.com>
2025-08-15 15:15:54 -0300

    wrote the map and the game loop

---
+++ main.py
@@ -0,0 +1,46 @@
+import pygame as py
+import map
+
+
+screen = py.display.set_mode((map.COLS * map.TILE_SIZE, map.ROWS * map.TILE_SIZE))
+py.display.set_caption("3d Engine lol") 
+
+
+
+white  = (255, 255, 255)
+black = (0, 0, 0)
+
+
+
+def draw_map():
+        for row in range(map.ROWS):
+            for col in range(map.COLS):
+                value = map.map[row][col]
+                if value == 0:
+                    py.draw.rect(screen, black, 
+                        (col * map.TILE_SIZE, row * map.TILE_SIZE, map.TILE_SIZE, map.TILE_SIZE), 
+                        1) #outline 
+
+                if value == 1:
+                    py.draw.rect(screen, white, 
+                        (col * map.TILE_SIZE, row * map.TILE_SIZE, map.TILE_SIZE, map.TILE_SIZE), 
+                        1)  
+                
+
+
+
+
+
+
+running = True
+while running:
+    for event in py.event.get():
+        if event.type == py.QUIT: 
+            running = False
+
+        screen.fill((0, 0, 0)) 
+        draw_map()
+    
+    py.display.flip() 
+
+
